fc8b5e46b6f81239243062d0adddf45a61d6c190
GilfanovD <GilfanovD@RU1104150771>
2016-10-07 19:23:36 +0300

    Add project files.

---
+++ MoviesFeedBacks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoviesFeedBacks.MvcApplication" Language="C#" %>
---
+++ MoviesFeedBacks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoviesFeedBacks.Startup))]
+namespace MoviesFeedBacks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
